aa51e107fbb677d7f112b4a350a44ec9f23638eb
JoachimDAVAL <joachim.daval01@gmail.com>
2026-09-04 21:19:02 +0200

    fix: scroll tot op on rout echange

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,9 +1,16 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import { Navbar } from '../components/layout/Navbar'
 import { Footer } from '../components/layout/Footer'
 import { CartDrawer } from '../components/cart/CartDrawer'
 
 export default function MainLayout() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
   return (
     <div className="vk-app">
       <Navbar />
